e2e2264cd7b72db0524cda5d0382f86b07808494
Han-Seunghun <ggzerosum@naver.com>
2020-06-04 14:18:51 +0900

    선택 정렬

---
+++ 선택 정렬/선택 정렬/선택 정렬/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace 선택_정렬
+{
+    class Program
+    {
+        private static int[] inputs = new[]
+        {
+            10,
+            3,
+            5,
+            20,
+            1,
+            2,
+            100,
+            77,
+            30,
+            4,
+            9
+        };
+
+        static void Main(string[] args)
+        {
+            SelectionSort(in inputs);
+
+            int index = 0;
+            foreach (int input in inputs)
+            {
+                Console.WriteLine($"[{index++}] : {input}");
+            }
+        }
+
+        static void SelectionSort(in int[] inputs)
+        {
+            for (int i = 0; i < inputs.Length - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < inputs.Length; j++)
+                {
+                    min = inputs[j] < inputs[min] ? j : min;
+                }
+
+                if (min != i)
+                    Swap(i, min, in inputs);
+            }
+        }
+
+        static void Swap(int indexA, int indexB, in int[] array)
+        {
+            int a = array[indexA];
+            array[indexA] = array[indexB];
+            array[indexB] = a;
+        }
+    }
+}
