df298e63c1ea4f00c3e71f8623abf5f05d866d2c
Han-Seunghun <ggzerosum@naver.com>
2020-07-06 16:28:10 +0900

    [분할정복] 7.2 수열의 빠른 합

---
+++ 알고리즘 문제해결 전략/Chapter 7 - 분할 정복 (Divide&Conquer)/수열의 빠른 합(p176)/수열의 빠른 합(p176/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace 수열의_빠른_합_p176
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = Algorithm.FastestSum(100);
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+
+    static class Algorithm
+    {
+        public static int FastestSum(int n)
+        {
+            if (n == 1)
+                return 1;
+
+            if (n % 2 != 0)
+                return FastestSum(n - 1) + n;
+
+            return 2 * FastestSum(n / 2) + (n/2 * n/2);
+        }
+    }
+}
