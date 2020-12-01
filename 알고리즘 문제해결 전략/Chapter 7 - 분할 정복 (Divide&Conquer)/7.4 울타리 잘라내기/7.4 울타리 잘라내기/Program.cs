899c46c655d0b73d541675b833bdc077ffaebfbc
Han-Seunghun <ggzerosum@naver.com>
2020-12-01 12:49:13 +0900

    7.4 울타리 잘라내기 -  1) 무식하게 풀기(Brute force)

---
+++ 알고리즘 문제해결 전략/Chapter 7 - 분할 정복 (Divide&Conquer)/7.4 울타리 잘라내기/7.4 울타리 잘라내기/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._4_울타리_잘라내기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinimumAreaOfFences minimumAreaOfFences = new MinimumAreaOfFences();
+            int maxArea = minimumAreaOfFences.GetMaxArea_BruteForce(new int[]
+            {
+                7, 1, 5, 9, 6, 7, 3
+            });
+
+            Console.WriteLine(maxArea);
+        }
+    }
+
+    internal class MinimumAreaOfFences
+    {
+        public int GetMaxArea_BruteForce(int[] heights)
+        {
+            /*
+             * 어떤 수들의 집합에서, 시작(S)과 끝(E) 사이의
+             * 
+             */
+
+            int count = heights.Length;
+            int maxArea = 0;
+            for (int left = 0; left < count; left++)
+            {
+                int minHeight = heights[left];
+                for (int right = left; right < count; right++)
+                {
+                    minHeight = Math.Min(minHeight, heights[right]);
+                    maxArea = Math.Max(maxArea, (right - left + 1) * minHeight);
+                }
+            }
+
+            return maxArea;
+        }
+    }
+}
