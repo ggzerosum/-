3f6c634204360ad273cbf43549a4acca8475b650
Han-Seunghun <ggzerosum@naver.com>
2020-12-15 23:54:40 +0900

    2748번 - 피보나치 수 2 (동적계획법) - for문을 사용하는 동적계획법 추가

--- 백준 알고리즘 문제/2748번 - 피보나치 수2/2748번 - 피보나치 수2/Program.cs
+++ 백준 알고리즘 문제/2748번 - 피보나치 수2/2748번 - 피보나치 수2/Program.cs
@@ -11,44 +11,60 @@ namespace _2748번___피보나치_수2
     {
         static void Main(string[] args)
         {
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
+            // For문을 활용한 동적계획법
+            int n = Convert.ToInt32(Console.ReadLine());
 
-            for (int N = 0; N < 18; N++)
+            if (n < 2)
             {
-                //int N = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(n);
+                return;
+            }
+
+            int f0 = 0;
+            int f1 = 1;
+            for (int i = 2; i <= n; i++)
+            {
+                int current = f1 + f0;
+                f0 = f1;
+                f1 = current;
+            }
+
+            Console.WriteLine(f1);
+        }
+
+        class UsingArray
+        {
+            public int Solve(int N)
+            {
                 int[] array = new int[N + 1];
                 for (int i = 0; i < N + 1; i++)
                 {
                     array[i] = -1;
                 }
-                Console.WriteLine(Fibonachi_Dynamic(array, N));
+
+                return Fibonachi_Dynamic(array, N);
             }
-
-            stopWatch.Stop();
-            Console.WriteLine(stopWatch.ElapsedMilliseconds);
-        }
-        
-        // 동적 계획법, 하위 부분 문제를 해결하고 하위 부분 문제의 해를 저장하여 다시 활용함으로써 시간을 줄이는 기법
-        private static int Fibonachi_Dynamic(int[] cache, int n)
-        {
-            if (n == 0)
-                return 0;
-
-            if (n == 1)
-                return 1;
-
-            if (cache[n] != -1)
+            // 동적 계획법, 하위 부분 문제를 해결하고 하위 부분 문제의 해를 저장하여 다시 활용함으로써 시간을 줄이는 기법
+            private int Fibonachi_Dynamic(int[] cache, int n)
             {
-                return cache[n];
-            }
-            else
-            {
-                cache[n - 1] = Fibonachi_Dynamic(cache, n - 1);
-                cache[n - 2] = Fibonachi_Dynamic(cache, n - 2);
+                if (n == 0)
+                    return 0;
 
-                cache[n] = cache[n - 1] + cache[n - 2];
-                return cache[n];
+                if (n == 1)
+                    return 1;
+
+                if (cache[n] != -1)
+                {
+                    return cache[n];
+                }
+                else
+                {
+                    cache[n - 1] = Fibonachi_Dynamic(cache, n - 1);
+                    cache[n - 2] = Fibonachi_Dynamic(cache, n - 2);
+
+                    cache[n] = cache[n - 1] + cache[n - 2];
+                    return cache[n];
+                }
             }
         }
     }
